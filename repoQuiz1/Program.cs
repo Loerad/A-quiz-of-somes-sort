f91813968ce887656ea0e7d2d55a8a3a01f1e5ed
JayleiAlderson-Muir <aldej2@student.op.ac.nz>
2022-09-16 14:59:38 +1200

    Added question 1

---
+++ repoQuiz1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace repoQuiz1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            const string answer1 = "elephant";
+            int score = 0;
+            Console.WriteLine("Question 1");
+            Console.Write("What is the world’s largest land mammal? ");
+            string guess1 = Console.ReadLine();
+            if (answer1 == guess1.ToLower())
+            {
+                Console.WriteLine("Congrats, you got question 1 correct! +1!");
+                score++;
+            } else
+            {
+                Console.WriteLine("That's too bad, the correct answer was: Elephant!");
+            }
+            Console.WriteLine($"Your score is: {score}");
+            Console.ReadLine();
+            Console.Clear();
+            Thread.Sleep(1000);
+        }
+    }
+}
