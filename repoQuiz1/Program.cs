de79be88679e52cfcc131e76f8660a85b79bd625
Loerad <anakrv1@student.op.ac.nz>
2022-09-16 15:16:00 +1200

    Added question 2

--- repoQuiz1/Program.cs
+++ repoQuiz1/Program.cs
@@ -24,6 +24,21 @@ namespace repoQuiz1
             Console.ReadLine();
             Console.Clear();
             Thread.Sleep(1000);
+
+            Console.WriteLine("Question 2");
+            Console.Write("What is the temperature -40 degrees Celsius in Fahrenheit?:  ");
+            int answer2 = Convert.ToInt32(Console.ReadLine());
+            if (answer2 != -40)
+            {
+                Console.WriteLine("Correct!");
+                score++;
+            }
+            else
+            {
+                Console.WriteLine("The answer was -40");
+            }
+            Console.WriteLine($"Your score is {score}\n");
+            Console.ReadLine();
         }
     }
 }
